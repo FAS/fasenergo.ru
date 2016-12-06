687590c437115905e86285aa40e7609c12d377fd
ArmorDarks <arkahor@gmail.com>
2016-12-06 02:23:43 +0200

    [js] update expand docs

--- source/scripts/plugins/expand.js
+++ source/scripts/plugins/expand.js
@@ -1,5 +1,39 @@
-// Displays target on `this` click and hides on another
-
+/**
+ * Displays target on `this` click and hides on another.
+ *
+ * To use, define `js-expand` class on element, which will control toggling of blocks.
+ * It's recommended to use `<button>` as controlling elements, since it will keep
+ * everything accessible for keyboards out of box.
+ * With default settings, set `js-expand__target` on element, which should be expanded.
+ * If scoping enabled, with default settings set `js-expand__scope` on any parent, to define
+ * scope, within which `js-expand` should search for targets to expand.
+ *
+ * Individual expand behaviour can be altered by setting following attributes
+ * on element, bearing `js-expand`:
+ *
+ * - data-expand='{{ your query }}':
+ *   - `false` or unspecified - toggles targets, defined by `userOptions.defaultTargetQuery` query.
+ *   - `your query` — query of a container, which should be expanded on this click.
+ *
+ * - data-expand-scope='{{ next || true || your query }}':
+ *   - `false` or unspecified — selects absolutely all targets defined by `data-expand` query
+ *   - `next` — selects all next siblings with defined in `data-expand` query.
+ *   - `true` — selects all targets defined by `data-expand` query within scope, defined by closest
+ *              container, which defined by `userOptions.defaultScopeQuery` query.
+ *   - `your query` — same as `true`, but you can specify specific query of defining scope container.
+ *
+ * @param {object} [userOptions]                                           Options overrides
+ * @param {number} [userOptions.duration]           = 600,                 Slide speed
+ * @param {string} [userOptions.easing]             = ''                   Slide easing animation
+ * @param {string} [userOptions.activeClass]        = 'is-active'          Class for active button
+ * @param {string} [userOptions.expandingClass]     = 'is-expanding'       Classing for expanding target
+ * @param {string} [userOptions.collapsingClass]    = 'is-collapsing'      Class for collapsing target
+ * @param {string} [userOptions.expandedClass]      = 'is-expanded'        Class for fully expanded target
+ * @param {string} [userOptions.defaultScopeQuery]  = '.js-expand__scope'  Default query of container defining scope
+ * @param {string} [userOptions.defaultTargetQuery] = '.js-expand__target' Default query of container to be expanded
+ *
+ * @return {object}             this
+ */
 export default $.fn.expand = function (userOptions) {
   const options = $.extend({
     duration: 600,
