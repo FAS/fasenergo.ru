ce00e94fba34417c7f4a464160a281408e91261b
ArmorDarks <arkahor@gmail.com>
2017-02-27 00:43:23 +0200

    [tests] fix paths to data

--- tests/data.test.js
+++ tests/data.test.js
@@ -7,10 +7,10 @@ import { join } from 'path'
 const { config, file: { readYAML } } = grunt
 
 const generatorsData = readYAML(join(config('path.source.data'), 'generators.yml'))
-const generatorsSchema = require(join('../../', config('path.source.data'), 'generators.schema'))
+const generatorsSchema = require(join('../', config('path.source.data'), 'generators.schema'))
 
 const enginesData = readYAML(join(config('path.source.data'), 'engines.yml'))
-const enginesSchema = require(join('../../', config('path.source.data'), 'engines.schema'))
+const enginesSchema = require(join('../', config('path.source.data'), 'engines.schema'))
 
 describe('Generators data', () => {
   it('should be readable', () => expect(generatorsData).toBeTruthy())
