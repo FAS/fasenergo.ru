70254bbc6e1edbe4e4475167f645cde7802b8a7b
ArmorDarks <arkahor@gmail.com>
2017-05-04 19:06:18 +0300

    [js] add `Element.closest` polyfill for IE

--- source/scripts/helpers.js
+++ source/scripts/helpers.js
@@ -11,3 +11,22 @@ export const replaceChildren = ($parent, $children) => {
   $parent.innerHTML = ''
   $parent.appendChild($fragment)
 }
+
+// Polyfill Element.closest for IE browsers
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
+
+/* global Element */
+if (window.Element && !Element.prototype.closest) {
+  Element.prototype.closest = function (s) {
+    const matches = (this.document || this.ownerDocument).querySelectorAll(s)
+    let i
+    let el = this
+
+    do {
+      i = matches.length
+      while (--i >= 0 && matches.item(i) !== el) {}
+    } while ((i < 0) && (el = el.parentElement))
+
+    return el
+  }
+}
