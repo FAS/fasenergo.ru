b157111da7c443b34206ad84413733ce59b1426e
ArmorDarks <arkahor@gmail.com>
2017-05-06 00:20:40 +0300

    [js] add `Element.matches` polyfill

--- source/scripts/helpers.js
+++ source/scripts/helpers.js
@@ -10,10 +10,13 @@ export const replaceChildren = ($parent, $children) => {
   $parent.appendChild($fragment)
 }
 
-// Polyfill Element.closest for IE browsers
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
-
+// =============
+// Polyfills
+// =============
 /* global Element */
+
+// Polyfill `Element.closest` for IE browsers
+// @source https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
 if (window.Element && !Element.prototype.closest) {
   Element.prototype.closest = function (s) {
     const matches = (this.document || this.ownerDocument).querySelectorAll(s)
@@ -27,4 +30,22 @@ if (window.Element && !Element.prototype.closest) {
 
     return el
   }
+}
+
+// Polyfill `Element.matches` for older browsers, including IE11 and lower
+// @source https://developer.mozilla.org/en/docs/Web/API/Element/matches
+if (!Element.prototype.matches) {
+  Element.prototype.matches =
+    Element.prototype.matchesSelector ||
+    Element.prototype.mozMatchesSelector ||
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.oMatchesSelector ||
+    Element.prototype.webkitMatchesSelector ||
+    function (s) {
+      const matches = (this.document || this.ownerDocument).querySelectorAll(s)
+      let i = matches.length
+
+      while (--i >= 0 && matches.item(i) !== this) {}
+      return i > -1
+    }
 }
