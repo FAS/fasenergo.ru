75e21a13d1537e74fafcf8cf10edfac56de93319
ArmorDarks <arkahor@gmail.com>
2017-05-08 23:55:56 +0300

    [js] add polyfill for `classList`

--- source/scripts/polyfill.js
+++ source/scripts/polyfill.js
@@ -1,4 +1,4 @@
-/* global Element */
+/* global Element, DOMTokenList */
 
 // This is sad file where tortured souls dwells
 
@@ -36,3 +36,62 @@ if (!Element.prototype.matches) {
       return i > -1
     }
 }
+
+// Polyfill Element.classList for IE9 and add missing features in IE10
+// @source https://developer.mozilla.org/en/docs/Web/API/Element/classList
+;(function () {
+  // helpers
+  var regExp = function (name) {
+    return new RegExp('(^| )' + name + '( |$)')
+  }
+  var forEach = function (list, fn, scope) {
+    for (var i = 0; i < list.length; i++) {
+      fn.call(scope, list[i])
+    }
+  }
+
+  // class list object with basic methods
+  function ClassList (element) {
+    this.element = element
+  }
+
+  ClassList.prototype = {
+    add: function () {
+      forEach(arguments, function (name) {
+        if (!this.contains(name)) {
+          this.element.className += this.element.className.length > 0 ? ' ' + name : name
+        }
+      }, this)
+    },
+    remove: function () {
+      forEach(arguments, function (name) {
+        this.element.className = this.element.className.replace(regExp(name), ' ')
+      }, this)
+    },
+    toggle: function (name) {
+      return this.contains(name) ? (this.remove(name), false) : (this.add(name), true)
+    },
+    contains: function (name) {
+      return regExp(name).test(this.element.className)
+    },
+    // bonus..
+    replace: function (oldName, newName) {
+      this.remove(oldName)
+      this.add(newName)
+    }
+  }
+
+  // IE8/9, Safari
+  if (!('classList' in Element.prototype)) {
+    Object.defineProperty(Element.prototype, 'classList', {
+      get: function () {
+        return new ClassList(this)
+      }
+    })
+  }
+
+  // replace() support for others
+  if (window.DOMTokenList && DOMTokenList.prototype.replace == null) {
+    DOMTokenList.prototype.replace = ClassList.prototype.replace
+  }
+})()
