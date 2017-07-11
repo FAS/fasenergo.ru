64bfdb444bbb0ca331aeebd1b0542acf043dd6a1
ArmorDarks <git@lavrins.com>
2017-07-12 00:18:30 +0300

    [scripts][templates] add additional Metrics goal trackers, closes https://trello.com/c/ZLb0AdtV/130-добавить-action-метрики

--- source/scripts/plugins/analytics.js
+++ source/scripts/plugins/analytics.js
@@ -15,6 +15,11 @@ const reachGoal = (goalId) => {
 
 const $orderBtns = document.querySelectorAll('.js-metrica-order')
 const $requestOfferBtns = document.querySelectorAll('.js-metrica-request-offer')
+const $contatcUsBtns = document.querySelectorAll('.js-metrica-contact-us')
+const $askQuestionBtns = document.querySelectorAll('.js-metrica-ask-question')
+const $callbackBtns = document.querySelectorAll('.js-metrica-callback')
+const $whereToBuyBtns = document.querySelectorAll('.js-metrica-where-to-buy')
+const $expandFilterBtns = document.querySelectorAll('.js-metrica-expand-filter')
 
 document.addEventListener('click', (e) => {
   const $target = e.target
@@ -25,4 +30,9 @@ document.addEventListener('click', (e) => {
 
   forEachTarget($orderBtns, () => reachGoal('click-order'))
   forEachTarget($requestOfferBtns, () => reachGoal('click-request-offer'))
+  forEachTarget($contatcUsBtns, () => reachGoal('click-contact-us'))
+  forEachTarget($askQuestionBtns, () => reachGoal('click-ask-question'))
+  forEachTarget($callbackBtns, () => reachGoal('click-callback'))
+  forEachTarget($whereToBuyBtns, () => reachGoal('click-where-to-buy'))
+  forEachTarget($expandFilterBtns, () => reachGoal('click-expand-filter'))
 })
