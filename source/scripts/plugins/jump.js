8f26e898de1c44ffe5eedea37abc11e4137e39f9
ArmorDarks <git@lavrins.com>
2017-08-18 21:43:39 +0300

    [scripts] refactor `jump.js` to avoid additional pitfalls with existing and non-existing target elements and hashes

--- source/scripts/plugins/jump.js
+++ source/scripts/plugins/jump.js
@@ -3,35 +3,53 @@
 import jump from 'jump.js'
 
 /**
- * Smooth jumps for anchors, which leads to hashtags.
- * Use `.js-smooth-jump` class on anchor to enable.
- *
+ * Smooth jumps to hashes from anchors and on page load
+ * Add `.js-smooth-jump` class and href with hash on anchor to enable.
  * @source http://callmecavs.com/jump.js/
- *
- * @return void
- *
+ * @return {void}
  * @example
  *   <a class='js-smooth-jump' href='#section'>Link to section</a>
  *   <div id='section'>Test</div>
  */
 export default () => {
-  const jumpToHash = (link) => {
-    const hrefHash = link && link.getAttribute('href').replace(/^.*?(#|$)/, '')
-    const hash = hrefHash || window.location.hash.substring(1)
-    const target = document.getElementById(hash)
+  const $smoothLinks = document.querySelectorAll('.js-smooth-jump')
 
-    if (target) jump(target)
+  /**
+   * Wrapper around `jump.js` to ensure that jumping occurs only to existing elements
+   * @param {Node|string} target Element to which jump should occur or valid query
+   * @return {void}
+   */
+  const jumpSafely = (target) => {
+    if (!target) return
+
+    // We hit the query, let's ensure queried element exists on page
+    if (typeof target === 'string') {
+      const $targets = document.querySelectorAll(target)
+      const $target = $targets[0]
+
+      if ($target) jump($target)
+      return
+    }
+
+    // Otherwise we received node, jump to it
+    jump(target)
   }
 
-  // Jump smoothly if on loaded page exists hash
-  // Note, that it won't work for page transitions :(
-  jumpToHash()
+  /**
+   * Smooth jumps from element, which has `href` with hash
+   * @return {void}
+   */
+  const jumpFrom = (target) => {
+    const hrefHash = target && target.getAttribute('href').replace(/^.*?(#|$)/, '')
+    jumpSafely(`#${hrefHash}`)
+  }
 
-  const $smoothLinks = document.querySelectorAll('.js-smooth-jump')
+  // Jump smoothly if url has hash on page load
+  // Note, that it won't work for page transitions :(
+  jumpSafely(window.location.hash)
 
   document.addEventListener('click', (e) => {
     const $target = e.target
-
-    $smoothLinks.forEach(($l) => $l.contains($target) && jumpToHash($l))
+    $smoothLinks.forEach(($l) => $l.contains($target) && jumpFrom($l))
   })
 }
