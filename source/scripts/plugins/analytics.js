a55943b67fbaee17b9ff152dd20c337932e3cc53
ArmorDarks <git@lavrins.com>
2017-09-25 02:01:45 +0300

    [scripts] try hard to add errors tracking via Google Analytics

--- source/scripts/plugins/analytics.js
+++ source/scripts/plugins/analytics.js
@@ -3,6 +3,28 @@
 const YA_COUNTER_ID = 20139793
 
 /**
+ * Send exception to Google Analytics
+ * @param  {Error|string} error    Exception object with stack or message properties, or error message
+ * @param  {string}       [source] Url where error was raised.
+ * @param  {number}       [lineno] Line number where error was raised.
+ * @param  {number}       [colno]  Column number for the line where the error occurred.
+ * @param  {boolean}      [fatal=false] Is exception was fatal
+ * @return {void}
+ */
+export const exception = (error, source, lineno, colno, fatal = false) => {
+  console.error(error)
+
+  try {
+    window.ga('send', 'exception', {
+      exDescription: `${(error && (error.stack || error.message)) || error}\n    @ ${source}:${lineno}:${colno}`,
+      exFatal: fatal
+      // 'appName': 'Application_Name',
+      // 'appVersion': '1.0'
+    })
+  } catch (err) {}
+}
+
+/**
  * Send event to Google Analytics or goal to Yandex Metrica
  * @param {string} category Typically the object that was interacted with (e.g. `Video`).
  *                          Only for Google Analytics.
@@ -14,11 +36,32 @@ const YA_COUNTER_ID = 20139793
 const event = (category, action, label, value) => {
   try {
     window.ga('send', 'event', category, action, label, value)
-  } catch (e) { console.error(e) }
+  } catch (err) { exception(err) }
 
   try {
     window[`yaCounter${YA_COUNTER_ID}`].reachGoal(action, { label, order_price: value })
-  } catch (e) { console.error(e) }
+  } catch (err) { exception(err) }
+}
+
+const originalOnerror = window.onerror
+
+/**
+ * Log any script error to Google Analytics.
+ * Third-party scripts without CORS will only provide "Script Error." as an error message.
+ * Modified https://stackoverflow.com/a/29552301
+ * @param  {string} [messageOrEvent] Error message.
+ * @param  {string} [source]         Url where error was raised.
+ * @param  {number} [lineno]         Line number where error was raised.
+ * @param  {number} [colno]          Column number for the line where the error occurred.
+ * @param  {Error}  [error]          Error Object.
+ * @return {boolean} When the function returns true, this prevents the firing of the default event handler.
+ */
+window.onerror = (messageOrEvent, source, lineno, colno, error) => {
+  exception(error || messageOrEvent, source, lineno, colno)
+
+  if (typeof originalOnerror === 'function') {
+    return originalOnerror(messageOrEvent, source, lineno, colno, error)
+  }
 }
 
 document.addEventListener('click', (e) => {
