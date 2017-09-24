8a8bfcd818607d423b802fa60a09915d211cfcd4
ArmorDarks <git@lavrins.com>
2017-09-24 21:04:38 +0300

    [scripts] try to send events to Google Analytics too

--- source/scripts/plugins/analytics.js
+++ source/scripts/plugins/analytics.js
@@ -3,16 +3,21 @@
 const YA_COUNTER_ID = 20139793
 
 /**
- * Send reach goal to analytics
- * @param {string} goalId Id of a goal
+ * Send event to Google Analytics or goal to Yandex Metrica
+ * @param {string} category Typically the object that was interacted with (e.g. `Video`).
+ *                          Only for Google Analytics.
+ * @param {string} action   Action name (e.g. `click-order`) or goal ID in case of Yandex.Metrica
+ * @param {string} [label]  For categorizing events (e.g. `Fall Campaign`)
  * @return void
  */
-const reachGoal = (goalId) => {
+const event = (category, action, label) => {
   try {
-    window[`yaCounter${YA_COUNTER_ID}`].reachGoal(goalId)
-  } catch (e) {
-    console.error(e)
-  }
+    window.ga('send', 'event', category, action, label)
+  } catch (e) { console.error(e) }
+
+  try {
+    window[`yaCounter${YA_COUNTER_ID}`].reachGoal(action)
+  } catch (e) { console.error(e) }
 }
 
 document.addEventListener('click', (e) => {
@@ -22,14 +27,14 @@ document.addEventListener('click', (e) => {
     if ($element.contains($target)) cb()
   })
 
-  forEachTarget(document.querySelectorAll('.js-metrica-order'), () => reachGoal('click-order'))
-  forEachTarget(document.querySelectorAll('.js-metrica-request-offer'), () => reachGoal('click-request-offer'))
-  forEachTarget(document.querySelectorAll('.js-metrica-contact-us'), () => reachGoal('click-contact-us'))
-  forEachTarget(document.querySelectorAll('.js-metrica-ask-question'), () => reachGoal('click-ask-question'))
-  forEachTarget(document.querySelectorAll('.js-metrica-callback'), () => reachGoal('click-callback'))
-  forEachTarget(document.querySelectorAll('.js-metrica-where-to-buy'), () => reachGoal('click-where-to-buy'))
-  forEachTarget(document.querySelectorAll('.js-metrica-expand-filter'), () => reachGoal('click-expand-filter'))
-  forEachTarget(document.querySelectorAll('.js-metrica-subscribe-news'), () => reachGoal('click-subscribe-news'))
+  forEachTarget(document.querySelectorAll('.js-metrica-order'), () => event('Product', 'click-order'))
+  forEachTarget(document.querySelectorAll('.js-metrica-request-offer'), () => event('Product', 'click-request-offer'))
+  forEachTarget(document.querySelectorAll('.js-metrica-contact-us'), () => event('Products', 'click-contact-us'))
+  forEachTarget(document.querySelectorAll('.js-metrica-ask-question'), () => event('Product', 'click-ask-question'))
+  forEachTarget(document.querySelectorAll('.js-metrica-callback'), () => event('Sidebar', 'click-callback'))
+  forEachTarget(document.querySelectorAll('.js-metrica-where-to-buy'), () => event('Sidebar', 'click-where-to-buy'))
+  forEachTarget(document.querySelectorAll('.js-metrica-expand-filter'), () => event('Products filter', 'click-expand-filter'))
+  forEachTarget(document.querySelectorAll('.js-metrica-subscribe-news'), () => event('Subscribe form', 'click-subscribe-news'))
 })
 
 /**
@@ -39,5 +44,5 @@ document.addEventListener('click', (e) => {
  * @return {void}
  */
 export const alreadySubscribed = (result, msg) => {
-  if (result === 'error' && msg.includes('уже подписаны')) reachGoal('already-subscribed')
+  if (result === 'error' && msg.includes('уже подписаны')) event('Subscribe form', 'already-subscribed', msg)
 }
