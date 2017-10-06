9675f4f9a30116db2aaea6317b2ff2659053e640
ArmorDarks <git@lavrins.com>
2017-10-06 17:46:17 +0300

    [modules] improve logging of errors in `grunt-yandex-market-language`

--- modules/grunt-yandex-market-language.js
+++ modules/grunt-yandex-market-language.js
@@ -3,14 +3,14 @@ const { cyan, red } = require('chalk')
 
 module.exports = ({ registerMultiTask, log, verbose, file: { write }, util: { pluralize } }) =>
   registerMultiTask('yandexMarketLanguage', 'Extract data from specified files with Gray Matter', function () {
-    const files = this.files.length
     const { data } = this.options({
       data: {}
     })
 
-    if (!files) return log.error('No files specified.')
+    if (!this.files.length) return log.error('No files specified.')
 
-    let count = 0
+    let processedFiles = 0
+    let errors = 0
 
     this.files.forEach((file) => {
       const { dest, src } = file
@@ -22,14 +22,18 @@ module.exports = ({ registerMultiTask, log, verbose, file: { write }, util: { pl
         const YML = yml(data)
         write(dest, YML.end({ pretty: true }))
       } catch (err) {
+        errors++
         return log.warn(err)
       }
 
-      count++
+      processedFiles++
       verbose.ok(`File ${cyan(dest)} created`)
     })
 
-    const failedFiles = files - count
+    const processedMessage = `${cyan(processedFiles)} ${pluralize(processedFiles, 'file/files')} created`
+    const failedMessage = `${errors ? `, ${red(errors)} failed` : ''}`
 
-    log[failedFiles ? 'error' : 'ok'](`${cyan(count)} ${pluralize(count, 'file/files')} created${failedFiles ? `, ${red(failedFiles)} failed` : ''}`)
+    log[errors ? 'error' : 'ok'](processedMessage + failedMessage)
+
+    if (errors) throw new Error(`${errors} ${pluralize(errors, 'error/errors')} has been encountered during creating YML files.\nSee log above for details.`)
   })
