e0ab6f8980dc7dcb6c5b6eba3607b19f9f330ac3
ArmorDarks <git@lavrins.com>
2017-12-03 16:24:05 +0200

    [data] add email formatter

--- source/data/formatters.js
+++ source/data/formatters.js
@@ -1,4 +1,15 @@
 /**
+ * Form email from contacts data
+ * @param {object} data Complex object with email data. See source/data/contacts.coffee
+ * @return {string} Email
+ * @example {{ email(CONTACTS.departments.sales.emails[0]) // -> `sale@fasenergo.ru` }}
+ */
+const email = (data) => {
+  if (!data) throw new Error('[email] undefined provided, while object with email expected')
+  return `${data.value[0]}@${data.value[1]}`
+}
+
+/**
  * Output fuel type as a word
  * @param {string} phases Fuel type id
  * @return {string} Fuel type in a word
@@ -47,6 +58,7 @@ const nunjucksExtensions = (env) => {
 }
 
 module.exports = {
+  email,
   fuelTypeToWord,
   coolingToWord,
   phasesToWord,
