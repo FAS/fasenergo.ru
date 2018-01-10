91fc23debf394c8c48d4e0e1a0739d862f3311eb
ArmorDarks <git@lavrins.com>
2018-01-11 00:58:22 +0200

    [tests] simplify test case even more

--- tests/kotsu/nunjucks-extensions/imageSize.test.js
+++ tests/kotsu/nunjucks-extensions/imageSize.test.js
@@ -4,19 +4,16 @@ import { renderString } from '../../utils/nunjucks'
 
 const render = (template, context = mockContext, parse) => renderString(template, context, parse)
 const mockContext = {
-  PATH: {
-    images: 'assets/images'
-  },
   SITE: {
     images: [
-      { 'name': '/assets/images/test.jpg', 'width': 100, 'height': 150 },
-      { 'name': '/assets/images/test2.png', 'width': 120, 'height': 170 },
+      { 'name': '/images/test.jpg', 'width': 100, 'height': 150 },
+      { 'name': '/images/test2.png', 'width': 120, 'height': 170 },
 
-      { 'name': '/assets/images/set.jpg', 'width': 2000, 'height': 2500 },
-      { 'name': '/assets/images/set@500.jpg', 'width': 500, 'height': 550 },
-      { 'name': '/assets/images/set@1000.jpg', 'width': 1000, 'height': 1500 },
+      { 'name': '/images/set.jpg', 'width': 2000, 'height': 2500 },
+      { 'name': '/images/set@500.jpg', 'width': 500, 'height': 550 },
+      { 'name': '/images/set@1000.jpg', 'width': 1000, 'height': 1500 },
 
-      { 'name': '/assets/nope@images/set@1000.jpg', 'width': 1000, 'height': 1000 }
+      { 'name': '/nope@images/set@1000.jpg', 'width': 1000, 'height': 1000 }
     ]
   }
 }
@@ -24,19 +21,19 @@ const mockContext = {
 describe('Nunjucks `imageSize()`', () => {
   it('should get image size', () => {
     expect(render(`
-      {% set image = imageSize('/' + PATH.images + '/test.jpg') %}
+      {% set image = imageSize('/images/test.jpg') %}
       <img src='{{ image.src }}' width='{{ image.width }}' height='{{ image.height }}'>
     `)).toMatchSnapshot()
   })
 
   it('should print srcset', () => {
     expect(render(`
-      {% set image = imageSize('/' + PATH.images + '/test.jpg') %}
+      {% set image = imageSize('/images/test.jpg') %}
       <img src='{{ image.src }}'
         srcset='{{ image.srcset() }}'
       >
 
-      {% set image = imageSize('/' + PATH.images + '/set.jpg') %}
+      {% set image = imageSize('/images/set.jpg') %}
       <img src='{{ image.src }}'
         srcset='{{ image.srcset() }}'
       >
