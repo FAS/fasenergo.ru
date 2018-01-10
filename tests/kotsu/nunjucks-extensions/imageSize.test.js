840159e0022502a6e4a18e748c077a0580d82d6c
ArmorDarks <git@lavrins.com>
2018-01-11 00:56:09 +0200

    [tests] remove no longer needed part of test

--- tests/kotsu/nunjucks-extensions/imageSize.test.js
+++ tests/kotsu/nunjucks-extensions/imageSize.test.js
@@ -5,21 +5,18 @@ import { renderString } from '../../utils/nunjucks'
 const render = (template, context = mockContext, parse) => renderString(template, context, parse)
 const mockContext = {
   PATH: {
-    images: 'assets/images',
-    build: {
-      root: 'build'
-    }
+    images: 'assets/images'
   },
   SITE: {
     images: [
-      { 'name': 'build/assets/images/test.jpg', 'width': 100, 'height': 150 },
-      { 'name': 'build/assets/images/test2.png', 'width': 120, 'height': 170 },
+      { 'name': '/assets/images/test.jpg', 'width': 100, 'height': 150 },
+      { 'name': '/assets/images/test2.png', 'width': 120, 'height': 170 },
 
-      { 'name': 'build/assets/images/set.jpg', 'width': 2000, 'height': 2500 },
-      { 'name': 'build/assets/images/set@500.jpg', 'width': 500, 'height': 550 },
-      { 'name': 'build/assets/images/set@1000.jpg', 'width': 1000, 'height': 1500 },
+      { 'name': '/assets/images/set.jpg', 'width': 2000, 'height': 2500 },
+      { 'name': '/assets/images/set@500.jpg', 'width': 500, 'height': 550 },
+      { 'name': '/assets/images/set@1000.jpg', 'width': 1000, 'height': 1500 },
 
-      { 'name': 'build/assets/nope@images/set@1000.jpg', 'width': 1000, 'height': 1000 }
+      { 'name': '/assets/nope@images/set@1000.jpg', 'width': 1000, 'height': 1000 }
     ]
   }
 }
