7e713dbdb8b0638d28db6f90bee031387aa6be6d
ArmorDarks <git@lavrins.com>
2018-02-23 00:16:55 +0200

    [data] update Vaporizers scheme

--- source/data/vaporizers.test.js
+++ source/data/vaporizers.test.js
@@ -14,7 +14,11 @@ const Vaporizers = r.EqualKeyAndProp('id')(t.dict(t.String, t.struct({
     performance: t.Number,
     heatType: t.enums.of(['жидкостный', 'электрический']),
     heatTransferType: t.enums.of(['прямой', 'непрямой']),
-    pressureType: t.maybe(t.enums.of(['низкое', 'среднее']))
+    pressureType: t.maybe(t.enums.of(['низкое', 'среднее'])),
+    selfHeating: t.maybe(t.Boolean)
+  }, { strict: true }),
+  enclosure: t.struct({
+    type: t.maybe(t.enums.of(['рама', 'шкаф', 'контейнер', 'кольцевое']))
   }, { strict: true })
 }, { name: 'Vaporizers', strct: true })))
 
