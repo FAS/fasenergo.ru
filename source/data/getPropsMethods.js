78208a3b3b7fecda1e35c4871bbb70b30b227992
ArmorDarks <git@lavrins.com>
2018-02-24 15:47:04 +0200

    [data] show only model name and max power in generators breadcrumb submenu

--- source/data/getPropsMethods.js
+++ source/data/getPropsMethods.js
@@ -6,14 +6,15 @@ module.exports = ({ config }) => {
   return {
     getGeneratorPageProps (id) {
       const g = s.selectGenerator(GENERATORS, id)
+      const maxPower = s.getGeneratorHighestPower(g)
 
       return {
         generatorId: id,
 
         title: g.meta.title,
         contentTitle: g.title,
-        navTitle: g.title,
-        breadcrumbTitle: g.title,
+        navTitle: `${g.model} (${maxPower} кВт)`,
+        breadcrumbTitle: `${g.model} (${maxPower} кВт)`,
 
         description: g.meta.description,
 
