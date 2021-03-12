ce9a022876441cebdb0b72b147e6d699a9d7d635
Jason Hyde <mrhyde@users.noreply.github.com>
2021-03-12 03:11:48 +0300

    [misc] fixed codestyle

--- source/scripts/plugins/mobile-menu-toggler.js
+++ source/scripts/plugins/mobile-menu-toggler.js
@@ -3,7 +3,7 @@
 // Show or hide mobile menu when clicked
 
 export default $.fn.mobileMenuToggler = function () {
-  const $target = $(".js-mobile-menu").first()
+  const $target = $('.js-mobile-menu').first()
 
   const toggle = () => $target.fadeToggle('fast')
 
